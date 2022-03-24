4978de8f371a56299854ee5b0a7cf6e414683a62
ZeframLou <zeframlou@gmail.com>
2022-03-23 17:45:18 -0700

    update mint-mph-alloc.py

--- scripts/mint-mph-alloc.py
+++ scripts/mint-mph-alloc.py
@@ -24,8 +24,8 @@ def main():
     dev_treasury = "0xfecBad5D60725EB6fd10f8936e02fa203fd27E4b"
     discretionary_fund = "0xA9B2a4F90d0eC7A7907C11e2a17eF345d04747bF"
     converter_role = 0x1cf336fddcc7dc48127faf7a5b80ee54fce73ef647eecd31c24bb6cce3ac3eef
-    mint_total = (246913.26 + 21666.67 + 28888.89 + 72222.20 + 12819.44 +
-                  101111.08 + 231111.04 + 231111.04 + 36111.10 + 7222.22 + 116097.19) * 1e18
+    mint_total = (237291.90 + 20822.39 + 27763.18 + 69407.95 + 12319.91 +
+                  97171.13 + 222105.44 + 222105.44 + 34703.98 + 6940.80 + 111573.28) * 1e18
 
     ######################################################################
     # Mint MPH
@@ -39,48 +39,48 @@ def main():
 
     # keep MPH in gov treasury
     # this includes:
-    # 2022 user incentives: 246913.26
-    # Bug bounties: 21666.67
-    # Strategic MPH sale: 28888.89
-    # Protocol-owned liquidity: 72222.20
+    # 2022 user incentives: 237291.90
+    # Bug bounties: 20822.39
+    # Strategic MPH sale: 27763.18
+    # Protocol-owned liquidity: 69407.95
 
     # send MPH to dev treasury
     # this includes:
-    # 88mph SA immediate unlock: 12819.44
-    mph.transfer(dev_treasury, 12819.44 * 1e18)
+    # 88mph SA immediate unlock: 12319.91
+    mph.transfer(dev_treasury, 12319.91 * 1e18)
 
     # 10-year vests:
-    # 88mph foundation: 101111.08
+    # 88mph foundation: 97171.13
     # Because Sablier doesn't support streaming to oneself, the tokens are sent to
     # a discretionary multisig, which will handle creating a stream to the gov treasury
-    mph.transfer(discretionary_fund, 101111.08 * 1e18)
+    mph.transfer(discretionary_fund, 97171.13 * 1e18)
 
     # stream MPH using Sablier
     # this includes:
     #
     # 4-year vests:
-    # Zefram: 231111.04
-    # Guillaume: 231111.04
-    # Szeth: 36111.10
-    # Mathieu: 7222.22
-    # 88mph SA: 116097.19
+    # Zefram: 222105.44
+    # Guillaume: 222105.44
+    # Szeth: 34703.98
+    # Mathieu: 6940.80
+    # 88mph SA: 111573.28
 
-    stream_total = (231111.04 + 231111.04 + 36111.10 +
-                    7222.22 + 116097.19) * 1e18
-    current_time = math.floor(time.time()) + 60 * 60  # one hour in the future
+    stream_total = (222105.44 + 222105.44 + 34703.98 +
+                    6940.80 + 111573.28) * 1e18
+    current_time = math.floor(time.time()) + 5 * 24 * 60 * 60  # 5 days in the future
     four_years = 4 * 365 * 24 * 60 * 60
 
     mph.approve(sablier.address, stream_total)
 
     sablier.createStream("0x5f350bF5feE8e254D6077f8661E9C7B83a30364e",
-                         round_amount_using_duration(231111.04 * 1e18, four_years), mph.address, current_time, current_time + four_years)
+                         round_amount_using_duration(222105.44 * 1e18, four_years), mph.address, current_time, current_time + four_years)
     sablier.createStream("0xAfD5f60aA8eb4F488eAA0eF98c1C5B0645D9A0A0",
-                         round_amount_using_duration(231111.04 * 1e18, four_years), mph.address, current_time, current_time + four_years)
+                         round_amount_using_duration(222105.44 * 1e18, four_years), mph.address, current_time, current_time + four_years)
     sablier.createStream("0x1bfD64aB61EACf714B2Aa37347057203f3AcA71f",
-                         round_amount_using_duration(36111.10 * 1e18, four_years), mph.address, current_time, current_time + four_years)
+                         round_amount_using_duration(34703.98 * 1e18, four_years), mph.address, current_time, current_time + four_years)
     sablier.createStream("0xEF7F2e81EA14538858d962df34eB1bFDa83da395",
-                         round_amount_using_duration(7222.22 * 1e18, four_years), mph.address, current_time, current_time + four_years)
-    sablier.createStream(dev_treasury, round_amount_using_duration(116097.19 * 1e18, four_years),
+                         round_amount_using_duration(6940.80 * 1e18, four_years), mph.address, current_time, current_time + four_years)
+    sablier.createStream(dev_treasury, round_amount_using_duration(111573.28 * 1e18, four_years),
                          mph.address, current_time, current_time + four_years)
 
     # revoke converter role
